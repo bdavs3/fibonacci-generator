ccccca5da0d90326262d079a17b8981861180b05
bdavs3 <bdavs3000@gmail.com>
2021-06-22 10:28:16 -0700

    Scaffold methods for fib.go

--- fib/fib.go
+++ fib/fib.go
@@ -5,3 +5,15 @@ type Generator struct{}
 func NewGenerator() *Generator {
 	return &Generator{}
 }
+
+func (g *Generator) Fibonacci(val int) int {
+	return -1
+}
+
+func (g *Generator) Memoized(val int) int {
+	return -1
+}
+
+func (g *Generator) Clear() {
+
+}

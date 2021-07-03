ab02e509be4f4e09a1e3a2daa1557f4a4774dea0
bdavs3 <bdavs3000@gmail.com>
2021-07-02 17:11:09 -0700

    Automate table creation logic

--- db/db.go
+++ db/db.go
@@ -3,17 +3,49 @@ package db
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
-const url = "postgres://localhost:5432/mydatabase"
+const url = "postgres://localhost:5432/postgres"
 
 func NewConnection() *pgxpool.Pool {
 	conn, err := pgxpool.Connect(context.Background(), url)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(fmt.Sprintf("Error connecting to db: %s", err.Error()))
+	}
+
+	if !TableExists("fib_memo", conn) {
+		CreateMemo("fib_memo", conn)
 	}
 
 	return conn
+}
+
+func TableExists(tbl string, conn *pgxpool.Pool) bool {
+	var exists bool
+	conn.QueryRow(
+		context.Background(),
+		fmt.Sprintf(
+			`SELECT EXISTS 
+			(
+				SELECT 1
+				FROM information_schema.tables 
+				WHERE table_name = '%s'
+			);`, tbl),
+	).Scan(&exists)
+
+	return exists
+}
+
+func CreateMemo(tbl string, conn *pgxpool.Pool) {
+	conn.Query(
+		context.Background(),
+		fmt.Sprintf(
+			`CREATE TABLE %s (
+				term int,
+				val int
+			);`, tbl),
+	)
 }
